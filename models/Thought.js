adb5dabed51f3c2e65bb7b8a19b7eecf8fe0deee
Christian Kester <christiankester77@gmail.com>
2023-02-21 13:32:31 -0500

    added reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,31 @@
 const mongoose = require("mongoose");
 
+reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.ObjectId,
+        default: mongoose.Types.ObjectId,
+    },
+
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timeStamp) => {
+            timeStamp.toLocaleString("en-US", { timeZone: "EST" });
+        },
+    },
+});
+
 const thoughtSchema = new mongoose.Schema({
     thoughtText: {
         type: String,
@@ -16,10 +42,12 @@ const thoughtSchema = new mongoose.Schema({
         },
     },
 
-    userName: {
+    username: {
         type: String,
         required: true,
     },
+
+    reactions: [reactionSchema],
 });
 const Thought = mongoose.model("thought", thoughtSchema);
 
