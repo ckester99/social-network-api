ccdd3bd1130a85ef9707db837223e14b9efbe625
Christian Kester <christiankester77@gmail.com>
2023-02-24 09:21:54 -0500

    added user delete routes

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Thought } = require("../models");
+const { findOneAndUpdate } = require("../models/User");
 
 router.get("/", async (req, res) => {
     try {
@@ -19,6 +20,43 @@ router.get("/:id", async (req, res) => {
         res.json(userData);
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        User.create(req.body);
+        res.send("User created successfully!");
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedUser = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { returnDocument: "after" });
+        if (updatedUser) {
+            res.send(`${updatedUser.name} updated successfully`);
+        } else {
+            throw new Error("User not found!");
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const deleteData = await User.deleteOne({ _id: req.params.id });
+        if (deleteData.deletedCount) {
+            res.send(`Userid: ${req.params.id} deleted successfully`);
+        } else {
+            throw new Error("User not found!");
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ message: err.message });
     }
 });
 
